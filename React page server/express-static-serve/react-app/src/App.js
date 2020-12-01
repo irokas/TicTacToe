0256aecd08806fa24136e6adcec447734ae5ae18
Iro Kasidiari <ykasidiari@gmail.com>
2020-12-01 14:32:50 +0200

    removed all unnecessary files and redundant code

--- React page server/express-static-serve/react-app/src/App.js
+++ React page server/express-static-serve/react-app/src/App.js
@@ -2,32 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./styles.css";
 
-export const App = (props) => {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    axios.get("/users.json").then((response) => {
-      setUsers(response.data);
-    });
-  }, []);
-
-  return (
-    <div>
-      <ul className="users">
-        {users.map((user) => (
-          <li className="user">
-            <p>
-              <strong>Name:</strong> {user.name}
-            </p>
-            <p>
-              <strong>Email:</strong> {user.email}
-            </p>
-            <p>
-              <strong>City:</strong> {user.address.city}
-            </p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+export const App = () => {
+  return <div></div>;
 };
