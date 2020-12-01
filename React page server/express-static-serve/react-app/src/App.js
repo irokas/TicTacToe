8c3e319e1ab47894761746122bc1a894c52883e4
Iro Kasidiari <ykasidiari@gmail.com>
2020-12-01 13:39:20 +0200

    served react app

--- React page server/express-static-serve/react-app/src/App.js
+++ React page server/express-static-serve/react-app/src/App.js
@@ -1,37 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./styles.css";
 
-export default class App extends React.Component {
-  state = {
-    users: [],
-  };
-  componentDidMount() {
-    axios.get("/users.json").then((response) => {
-      this.setState({ users: response.data });
-    });
-  }
+export const App = (props) => {
+  const [users, setUsers] = useState([]);
 
-  render() {
-    const { users } = this.state;
-    return (
-      <div>
-        <ul className="users">
-          {users.map((user) => (
-            <li className="user">
-              <p>
-                <strong>Name:</strong> {user.name}
-              </p>
-              <p>
-                <strong>Email:</strong> {user.email}
-              </p>
-              <p>
-                <strong>City:</strong> {user.address.city}
-              </p>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    axios.get("/users.json").then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <ul className="users">
+        {users.map((user) => (
+          <li className="user">
+            <p>
+              <strong>Name:</strong> {user.name}
+            </p>
+            <p>
+              <strong>Email:</strong> {user.email}
+            </p>
+            <p>
+              <strong>City:</strong> {user.address.city}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
