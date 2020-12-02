5b88b540f15c4730df290b3be4698a26284345ba
Iro Kasidiari <ykasidiari@gmail.com>
2020-12-02 12:38:01 +0200

    Change expected tag to td

--- react-app/src/__tests__/square-test.js
+++ react-app/src/__tests__/square-test.js
@@ -5,9 +5,9 @@ import Square from "../Square";
 
 describe("Square test", function () {
   it("should render without throwing an error", () => {
-    expect(
-      shallow(<Square />).contains(<button className="square"></button>)
-    ).toBe(true);
+    expect(shallow(<Square />).contains(<td className="square"></td>)).toBe(
+      true
+    );
   });
 
   it('should be selectable by class "square"', () => {
