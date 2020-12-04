e9b88834e4e5953534e53ff279f1210a2accb7f7
Iro Kasidiari <ykasidiari@gmail.com>
2020-12-04 18:05:33 +0200

    Grid functions tests

---
+++ react-app/src/__tests__/grid-function-test.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import { Grid } from "../Grid";
+import { Square } from "../Square";
+
+jest.useFakeTimers();
+const grid = shallow(<Grid />);
+
+const gridJs = require("../Grid.js");
+
+describe("Tests the functions inside Grid.js", () => {
+  it("should call setTimeout", () => {
+    grid.find("#PvC").simulate("click");
+    grid.find(Square).at(1).simulate("click");
+
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+  });
+  //  findEmptyCells tests
+  it("should test findEmptyCells called with an empty array with length = 9", () => {
+    expect(gridJs.findEmptyCells(Array(9).fill("")).length).toBe(9);
+  });
+  it("should test findEmptyCells when called with non-empty array", () => {
+    let squares = Array(9).fill("");
+    squares[6] = "X";
+    squares[3] = "O";
+    expect(gridJs.findEmptyCells(squares).length).toBe(7);
+  });
+  it("should test findEmptyCells when called with full array", () => {
+    const filledSquares = Array(9).fill("X");
+    expect(gridJs.findEmptyCells(filledSquares).length).toBe(0);
+  });
+  //  declareTie tests
+  it("should declare tie", () => {
+    const tieSquares = ["X", "O", "X", "X", "O", "X", "O", "X", "O"];
+    expect(gridJs.declareTie(tieSquares)).toBe(true);
+  });
+  //  declareWinner tests
+  it("should not declare winner", () => {
+    const notWinnerTests = ["X", "O", "X", "X", "O", "X", "O", "X", "O"];
+    expect(gridJs.declareWinner(notWinnerTests, 7)).toBe(false);
+  });
+  it("should declare winner", () => {
+    const xWinnerSquares = ["X", "O", "X", "O", "O", "X", "O", "X", "X"];
+    expect(gridJs.declareWinner(xWinnerSquares, 8)).toBe(true);
+  });
+});
