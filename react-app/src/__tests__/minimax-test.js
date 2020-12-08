de14fa8829d052bc9ae585f42b9fe4036a4773c5
Iro Kasidiari <ykasidiari@gmail.com>
2020-12-08 16:50:52 +0200

    Added second minimax test with given board

--- react-app/src/__tests__/minimax-test.js
+++ react-app/src/__tests__/minimax-test.js
@@ -20,4 +20,23 @@ describe("Test the minimax algorithm", () => {
     }
     expect(bestMove).toBe(6);
   });
+  it("should return cell #8", () => {
+    const board = ["X", "O", "O", "", "X", "", "", "X", ""];
+    let bestScore = -2;
+    let bestMove = -1;
+    let newBoard = board.slice();
+
+    const emptyCells = gridJs.findEmptyCells(board);
+
+    for (let i = 0; i < emptyCells.length; i += 1) {
+      newBoard[emptyCells[i]] = "O";
+      const score = gridJs.minimax(false, newBoard, 0);
+      newBoard[emptyCells[i]] = "";
+      if (score > bestScore) {
+        bestScore = score;
+        bestMove = emptyCells[i];
+      }
+    }
+    expect(bestMove).toBe(8);
+  });
 });
