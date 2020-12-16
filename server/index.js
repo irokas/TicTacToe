489225d6435a323792a7646edffd37efbfed9415
Iro Kasidiari <ykasidiari@gmail.com>
2020-12-16 12:57:22 +0200

    set express port

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+
 const app = express();
 const path = require("path");
+
+const port = process.env.port || 5000;
 
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
-app.listen(5000, () => {
-  console.log("server started on port 5000");
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
 });
