263ba8205dbad3421b50a105fac8d6289c741367
Iro Kasidiari <ykasidiari@gmail.com>
2020-12-16 13:22:32 +0200

    Changed port to PORT

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const port = process.env.port || 5000;
+const PORT = process.env.port || 5000;
 
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
-app.listen(port, () => {
-  console.log(`server started on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
