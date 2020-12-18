2b7a8519492e6284c8de31e1654b57a928c68e57
Iro Kasidiari <ykasidiari@gmail.com>
2020-12-18 15:56:16 +0200

    Changed paths

--- server/index.js
+++ server/index.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const path = require("path");
 
-const environment = process.env.NODE_ENV || "development"; // if something else isn't setting ENV, use development
-const database = require("knex")(configuration); // connect to DB via knex using env's settings
-const configuration = require("../knexfile")[environment]; // require environment's settings from knexfile
-
 const app = express();
 
 const PORT = process.env.PORT || 5000;
