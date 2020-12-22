71b4b9df9d3b277f6e1c79f78b153203899ccf6d
Iro Kasidiari <ykasidiari@gmail.com>
2020-12-22 15:53:10 +0200

    Fixed yarnclean errors

--- jestconfig.js
+++ jestconfig.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import enableHooks from "jest-react-hooks-shallow";
-
+import "regenerator-runtime/runtime";
 // pass an instance of jest to `enableHooks()`
 enableHooks(jest);
