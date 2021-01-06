09a2e44e8aef408b6c3d51197eac371a2b2bde68
Iro Kasidiari <ykasidiari@gmail.com>
2021-01-06 11:54:50 +0200

    changed dev vars

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: "pg",
-    connection: "postgres://localhost/results",
+    connection: process.env.DATABASE_URL || "postgres://localhost/results",
     migrations: {
       directory: "./db/migrations",
     },
