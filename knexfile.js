322102abe16d61a525f3e2d3e4552cdc947315cf
Iro Kasidiari <ykasidiari@gmail.com>
2021-01-06 10:57:59 +0200

    change connection variable on production

--- knexfile.js
+++ knexfile.js
@@ -12,7 +12,8 @@ module.exports = {
 
   production: {
     client: "postgres",
-    connection: process.env.DATABASE_URL,
+    connection:
+      "postgres://rvyndmeitruxxa:aa09cc19ca085f350eab4b7f86401019cf1c5425556ea39a5a4163af72b10cee@ec2-3-251-0-202.eu-west-1.compute.amazonaws.com:5432/d8tovifnpgq6ff",
     migrations: {
       directory: "./db/migrations",
     },
