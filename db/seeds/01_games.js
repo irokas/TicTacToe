03ca38ce69a41a87466dc2888232df39e95b00c0
Iro Kasidiari <ykasidiari@gmail.com>
2021-01-15 13:01:25 +0200

    change games seed file

--- db/seeds/01_games.js
+++ db/seeds/01_games.js
@@ -2,6 +2,6 @@ exports.seed = (knex) => {
   return knex("games")
     .del()
     .then(() => {
-      return knex("games").insert([{ board: "X,O,,X,O,,X,,", user_id: 4 }]);
+      return knex("games").insert([{ board: "X,O,,X,O,,X,,", user_id: 2 }]);
     });
 };
