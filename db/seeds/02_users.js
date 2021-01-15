a3ee8cf6508aa52d1b0f8fca47bb9bba065ee8f2
Iro Kasidiari <ykasidiari@gmail.com>
2021-01-15 13:01:01 +0200

    change user seed file

--- db/seeds/02_users.js
+++ db/seeds/02_users.js
@@ -2,8 +2,12 @@ exports.seed = (knex) => {
   return knex("users")
     .del()
     .then(() => {
-      return knex("users").insert([
-        { username: "irokasidiari", password: "superpassword" },
+      return knex('users').insert([
+        {
+          username: 'irokasidiari',
+          password:
+            '$2b$10$rWkjEz1nMpasxVqRMoQuVOR3jsQdt8e2W/130odWVBJIRxVC2VtZG',
+        },
       ]);
     });
 };
